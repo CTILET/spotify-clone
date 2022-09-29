6a196d527bfa2c79cab3fd4f5bc14119fe564f56
CTILET <ctilet757@gmail.com>
2022-09-29 22:33:39 +0500

    shazam core api added

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,24 @@
+import { fromJSON } from "postcss"
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: () => {
+      headers.set(
+        "X-rapidAPI-key",
+        "6482d5961dmsh99d77743767b247p1cc87fjsn469b6d01470b"
+      )
+
+      return headers
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: "/charts/world" }),
+  }),
+})
+
+export const {
+   useGetTopChartsQuery,
+} = shazamCoreApi;
